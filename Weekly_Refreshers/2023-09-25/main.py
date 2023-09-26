8c990ac81608e6d04f12ca966dda8a692a176e86
Nafiz Sheikh <nafizsheikh47@gmail.com>
2023-09-26 16:32:18 -0400

    2023-09-26 Interval Commit

---
+++ Weekly_Refreshers/2023-09-25/main.py
@@ -0,0 +1,4 @@
+from modules import GetDates
+
+if __name__ == "__main__":
+    print("Hello World!")
---
+++ Weekly_Refreshers/2023-09-25/modules.py
@@ -0,0 +1,5 @@
+# Import requisitive packages
+
+class GetDates:
+    def __init__(self):
+        pass
