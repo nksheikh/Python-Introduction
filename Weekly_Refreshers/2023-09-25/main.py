ff9634e5fd211f8f37065eec14fe8cb870873f85
Nafiz Sheikh <nafizsheikh47@gmail.com>
2023-09-28 14:46:54 -0400

    2028-09-28 Commit

--- Weekly_Refreshers/2023-09-25/main.py
+++ Weekly_Refreshers/2023-09-25/main.py
@@ -1,4 +1,10 @@
+# Import the requisite packages
+from datetime import datetime, timezone
 from modules import GetDates
 
+# Main execution point
 if __name__ == "__main__":
-    print("Hello World!")
+    
+    # Create date obtainer and print current time in timezones
+    dateobtainer = GetDates()
+    dateobtainer.printdates(timezones = [timezone.utc])
--- Weekly_Refreshers/2023-09-25/modules.py
+++ Weekly_Refreshers/2023-09-25/modules.py
@@ -1,5 +1,18 @@
 # Import requisitive packages
+from datetime import datetime, timezone
 
 class GetDates:
+    
+    # Class constructor
     def __init__(self):
         pass
+    
+    # Print current date
+    def printdates(self, timezones):
+        
+        # Loop through all desired timezones and print the current time in each timezone
+        for tz in timezones:
+            
+            # Obtain and print the current time in the timezone
+            currentdatetime = datetime.now(tz = tz)
+            print(currentdatetime)
